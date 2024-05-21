04a1d43aeaf0398785587798b00ad4281ecb241b
KJT <kevin.jeanot@gmail.com>
2024-05-21 15:08:04 +0200

    Project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { NgForOf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlist';
+  items : WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffe', true),
+    new WishItem('être embauché')
+  ]
+  title = 'my wishlist';
+  toggleItem = (item : WishItem) => {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  };
 }
